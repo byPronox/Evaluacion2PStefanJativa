c48f2ec599b59d79995ec5a72d0c17de2aafb483
byPronox <bypronoxyt@gmail.com>
2024-12-03 07:27:47 -0500

    Commit 2 Creacion de modelos

---
+++ Evaluacion 2P Stefan Jativa/Models/Recarga.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion_2P_Stefan_Jativa.Models
+{
+    public class Recarga
+    {
+        public string NumeroTelefono { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Fecha { get; set; }
+    }
+}
